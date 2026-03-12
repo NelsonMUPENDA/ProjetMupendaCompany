aa58ca8a0ef6af1c2bed321227b143bb78985dfd
NelsonMupenda <nelsonmupenda243@gmail.com>
2026-03-12 15:33:15 +0200

    Préparation de l'application pour la mise en ligne

--- MupendaCompany/settings.py
+++ MupendaCompany/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -11,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&uc6l=(%@3y8$!h_8qr34-nz+^klnq3g=y4ag)^e2o17!#pil2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['mupenda.cd', 'www.mupenda.cd']
 
 # Application definition
 
@@ -129,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATICFILES_DIRS = [
 #     BASE_DIR / "AppMupenda/static",
 # ]
