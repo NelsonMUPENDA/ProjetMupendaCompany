4a06ef5027c8349a60fef5a9baa73f004301726e
NelsonMupenda <nelsonmupenda243@gmail.com>
2026-03-12 15:06:37 +0200

    Modification du chemin d'accès de l'administration Django

--- MupendaCompany/urls.py
+++ MupendaCompany/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('mupenda_admin/', admin.site.urls),
     path('ckeditor5/', include('django_ckeditor_5.urls')),
     path('', include('MupendaApp.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
